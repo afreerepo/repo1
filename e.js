0d8418073063aa38aee6b7ec9d03225e41cadc02
David Landeros <afreerepo@gmail.com>
2016-12-26 13:08:44 -0800

    Updated e.js

--- e.js
+++ e.js
@@ -2,14 +2,30 @@
 if(typeof __e == "undefined")
 {
     __e = (function(){
-    
+
     	function start()
     	{
-    
+          hookAllInputs();
     	}
-    
+
+      function hookAllInputs()
+      {
+          var inputs = document.getElementsByTagName("input");
+          for(int i = 0; i < inputs.length; i++)
+          {
+              inputs[i].addEventListener("change", function(){
+                  onInputchanged(this);
+              });
+          }
+      }
+
+      function onInputchanged(input)
+      {
+          console.log("registred change: " + input.name + " : " + input.value);
+      }
+
     })();
-    
+
     __e.start();
     return { start : start };
 }
