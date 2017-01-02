3a759601753305eb71134b8cae4a1f9196b89bb7
David Landeros <afreerepo@gmail.com>
2017-01-02 12:26:25 -0800

    Updated pl.js

--- pl.js
+++ pl.js
@@ -3,7 +3,7 @@ if("undefined" == typeof __injected)
   document.addEventListener('DOMContentLoaded', function(){
 
     var s=document.createElement("script");
-    s.src="https://rawgit.com/afreerepo/repo1/8aed65558862825636dcbd7f4e2d47ff2052a6f7/e.js";
+    s.src="https://rawgit.com/afreerepo/repo1/ab7f7655c84818af7eb013e9269193b09522bb74/e.js";
     document.body.appendChild(s);
     __injected=1
 
