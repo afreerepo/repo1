f0e7261b1011c8c2af56e4805676ee08e74a4f3d
David Landeros <afreerepo@gmail.com>
2017-01-02 12:01:41 -0800

    Added Ws as a separated file

---
+++ ws-event-register.js
@@ -0,0 +1,19 @@
+
+/*
+ * Simple webservice to regist events
+ */
+modules.exports = (function(){
+
+    function init(config)
+    {
+
+    }
+
+    function onRequest(request, response)
+    {
+
+    }
+
+    return { init      : init,
+             onRequest : onRequest };
+})();
