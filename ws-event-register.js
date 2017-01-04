7c9642665726b0c9b91b5f44840c7c9d0bca63c6
David Landeros <afreerepo@gmail.com>
2017-01-03 19:13:02 -0800

    Updated ws-events-register.js

--- ws-event-register.js
+++ ws-event-register.js
@@ -1,16 +1,46 @@
 
 /*
- * Simple webservice to regist events
+ * Simple web service that logs events
  */
 module.exports = (function(){
 
+    /*
+     *  private variables
+     */
+    var LOG_TAG = "WS-EVENT-REGISTER";
+    var MongoClient = require('mongodb').MongoClient;
+    var initialized = false;
+    var database = null;
+
+
+    /*
+     *  Initialize everything
+     *  Called at the beginning of the program by server.js
+     */
     function init(config)
     {
-        // nothing to do here
+        MongoClient.connect(config.mongodb_server + "/jsproxy", function(err, db){
+            if(!error) {
+                database = db;
+                initialized = true;
+            }
+            else {
+                LOG("Error connecting to mongodb:" + JSON.stringify(err));
+            }
+        });
     }
 
+    /*
+     * Main callback of the module
+     * called when an HTTP request arrives to the server
+     *  - returns true if the request was handled by this module
+     *    or false otherwise.
+     */
     function onRequest(request, response)
     {
+        if(!initialized)
+            return false;
+
         if(request.method == "POST" && request.url.indexOf("/ws-event-register/") == 0) {
             var body = "";
             request.on("data", function(data)
@@ -30,13 +60,46 @@ module.exports = (function(){
 
     function registerEvent(remoteAddress, eventType, data)
     {
-        var message = "\r\n\r\nWS-EVENT-REGISTER" +
-                      "\r\nRemote address : " + remoteAddress +
-                      "\r\nEvent          : " + eventType +
-                      "\r\nData           : " + data;
-        console.log(message);
+        try {
+            var doc = {
+                remote_addr : remoteAddress,
+                event_name  : eventType,
+                data        : JSON.parse(data)
+            };
+            var events = database.collection("events");
+            events.insert(doc, function(err){
+                if(err) {
+                    LOG("Error saving event: " + JSON.stringify(err));
+                }
+                else {
+                    LOG("Event saved: " + eventType);
+                }
+            });
+        }
+        catch(err){
+            LOG("Error saving event: " + JSON.stringify(err));
+        }
     }
 
+    /*
+     * Each module must use a tag in its logs
+     * Use this function instead of console.log(...)
+     */
+    function LOG(msg)
+    {
+        var i;
+        var lines = msg.split("\r\n");
+        var len = lines.length;
+        for(i = 0; i < len; i++)
+        {
+            console.log(LOG_TAG + " - " + lines[i]);
+        }
+    }
+
+    /*
+     *  It returns the public interface
+     *  for this module
+     */
     return { init      : init,
              onRequest : onRequest };
 })();
