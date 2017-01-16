e51e8faa0e40eef73125d73702ce7d14c95a2e68
David Landeros <afreerepo@gmail.com>
2017-01-16 10:31:38 -0800

    Updated ws-event-register.js

--- ws-event-register.js
+++ ws-event-register.js
@@ -7,19 +7,21 @@ module.exports = (function(){
     /*
      *  private variables
      */
-    var LOG_TAG = "WS-EVENT-REGISTER";
-    var MongoClient = require('mongodb').MongoClient;
-    var URL = require("url");
+    var LOG_TAG     = "WS-EVENT-REGISTER";
+    var MongoClient = null;
+    var URL         = null;;
     var initialized = false;
-    var database = null;
-
+    var database    = null;
 
     /*
      *  Initialize everything
      *  Called at the beginning of the program by server.js
      */
-    function init(config)
+    function init(config, root_require_func)
     {
+        URL = root_require_func("url");
+        MongoClient = root_require_func('mongodb').MongoClient;
+        
         MongoClient.connect(config.mongodb_server + "/jsproxy", function(err, db){
             if(!err) {
                 database = db;
@@ -31,17 +33,31 @@ module.exports = (function(){
         });
     }
 
-    /*
-     * Main callback of the module
-     * called when an HTTP request arrives to the server
-     *  - returns true if the request was handled by this module
-     *    or false otherwise.
-     */
+    /**
+      * @api {post} /ws-event-register/<EventName> Logs an event
+      * @apiName Event register
+      * @apiGroup spyjs
+      * @apiParam {String} EventName The event to be logged
+      *
+      * @apiParamExample {json} Request-Example:
+      *     POST /ws-event-register/Inputchanged
+      *     Host: example.com
+      *     Content-Length: 47
+      *     Content-Type: application/json
+      *
+      *     { "name" : "password", "value" : "MartyMcFly" }
+      *
+      * @apiSuccessExample {json} Success-Response:
+      *     HTTP/1.1 200 OK
+      *     Content-Length: 20
+      *     Content-Type: application/json
+      *
+      *     { "success" : true }
+      */
     function onRequest(request, response)
     {
         if(!initialized)
             return false;
-
         if(request.method == "POST" && request.url.indexOf("/ws-event-register/") == 0) {
             var body = "";
             request.on("data", function(data)
